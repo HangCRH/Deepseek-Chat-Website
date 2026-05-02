c5197ef8f979241912ea76b4b0dbd4fddcaad393
HangCRH <1317806770@qq.com>
2026-05-02 12:07:09 +0800

    修复前端版本号，更改前端FETCH_URL格式，动态获取模型列表，更改前端消息格式验证机制及深度思考内容检测

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,43 @@
-const WEBSITE_VERSION = "1.2.0";
+const WEBSITE_VERSION = "1.3.0";
 var dialogHistory = [];  // 用于存储对话上下文的数组
 var isFirstMessage = true;  // 标记是否是第一条消息(用于在第一天消息时清空欢迎语)
 var animationInterval;
-const FETCH_URL = "http://localhost:32767/ds/";  //向服务器请求的URL
+const FETCH_URL = "http://localhost:32767/";  //向服务器请求的根URL
+
+function getModelList() {   //初始化模型列表，网页加载后立即执行
+    function writeModelSelect(modelList) {  //处理获取的模型列表
+        var modelSelector = document.getElementById("modelSelect");
+        modelSelector.innerHTML = "";
+        for (let i = 0; i < modelList.length; i++) {
+            var option = document.createElement("option");
+            option.value = modelList[i].name;           //实际使用的模型名称，发送给后端
+            option.text = modelList[i].display_name;    //展示给用户看的模型名称
+            modelSelector.appendChild(option);          //添加至模型列表选择框
+        }
+    }
+    fetch(FETCH_URL + "ds/modellist/")      //获取模型列表
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("获取模型列表成功，返回数据：", data);
+            writeModelSelect(data);
+        })
+        .catch((err) => {
+            console.error("获取模型列表失败：", err);
+            alert("获取模型列表失败，请刷新页面重试。若问题依旧，请联系开发者，并提供金肯详细的信息。Err0\n" + err);
+        });
+}
+document.addEventListener("DOMContentLoaded", getModelList);  //在页面加载完成后执行获取模型列表
 function sendRequest() {
     console.log("开始发送请求");
     document.getElementById("sendbtn").disabled = true; // 在发送请求时禁用发送按钮，防止重复点击
-    var chosenModel = document.getElementById("modelSelect").value;
+    var useModel = document.getElementById("modelSelect").value;
     var message = document.getElementById("userInput").value;
-    var useModel;
-    switch (chosenModel) {
-        case "v3.2chat":
-            useModel = "deepseek-chat";
-            break;
-        case "v3.2reasoner":
-            useModel = "deepseek-reasoner";
-            break;
-        default:
-            console.log("未知模型");
-            break;
-    }
     console.log(useModel, message);
+    if (useModel == "waiting") {    // 如果模型列表还在加载中，提示用户稍后再试
+        alert("模型列表加载中，请稍候再试！");
+        document.getElementById("sendbtn").disabled = false;
+        return;
+    }
     dialogHistory.push({ "role": "user", "content": message });  // 将用户输入添加到对话历史中
     console.log("当前对话历史：", dialogHistory);
     // 如果是第一条消息，清空欢迎语
@@ -37,7 +54,7 @@ function sendRequest() {
     // 添加动画
     animationInterval = setInterval(WaitingAnimation, 500);
     // 发送请求
-    fetch(FETCH_URL, {
+    fetch(FETCH_URL + "ds/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -74,12 +91,12 @@ function WaitingAnimation() {
     }
 }
 function processResponse(responseData, useModel) {
-    if (responseData.error || !responseData.choices[0]) {
+    if (responseData.error || !responseData.choices[0].message) {
         console.log("后端返回错误。responseData：" + responseData);
         alert("生成答复时出错。Err2\n" + responseData);
         return;
     }
-    if (useModel == "deepseek-reasoner") {
+    if (responseData.choices[0].message.reasoning_content) {    //有深度思考内容，要显示出来
         if (responseData.choices[0].message && responseData.choices[0].message.content) {
             var thinkingPart = responseData.choices[0].message.reasoning_content;   // 获取思考过程
             // 创建思考过程的标题
@@ -96,6 +113,10 @@ function processResponse(responseData, useModel) {
         var answerPart = responseData.choices[0].message.content;
         dialogHistory.push({ "role": "assistant", "content": answerPart });  // 将AI的回答添加到对话历史中
         console.log("ai回答后的对话历史：", dialogHistory);
+    } else {
+        console.log("后端返回错误。responseData：" + responseData);
+        alert("生成答复时出错。Err3\n" + responseData);
+        return;
     }
     // 创建回答正文元素
     var answerElement = document.createElement("p");
@@ -145,14 +166,11 @@ window.onbeforeunload = function () {
     }
 }
 function outMenu() {
-    var alertstr = "版本：1.2.0\n\n" +
+    var alertstr = "版本：" + WEBSITE_VERSION + "\n\n" +
         "更新内容：\n" +
-        "1、支持连续对话。\n" +
-        "    ·添加“开启新对话”按钮。\n" +
-        "2、在模型回答完毕后清空输入框。\n" +
-        "3、在关闭或刷新时提示用户确认，避免误操作导致对话历史丢失。\n" +
-        "4、部分技术性更新。\n\n" +
-        "联系开发者：\n" +
+        "1、适配deepseek-v4。\n" +
+        "2、模型列表动态获取。\n" +
+        "\n联系开发者：\n" +
         "邮箱：1317806770@qq.com\n";
     alert(alertstr);
 }
